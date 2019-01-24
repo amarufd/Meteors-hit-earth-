eeb3e4758e1c6a85cf477af429af70343527dd0c
Jpvanreckless <juan.lorcag@utem.cl>
2019-01-23 23:06:29 -0300

    Se comienza a trabajar

---
+++ AMSmeteorsfinal.R
@@ -0,0 +1,49 @@
+#==================== AMS.METEORS.ORG ====================#
+
+##########################################################
+######### Iniciando la extracción de información #########
+##########################################################
+
+# Se asigna la página de ams.meteors.org/firelogs
+webpage<- 'https://www.amsmeteors.org/about/'
+
+webpage <- read_html(webpage)
+
+# Extrayendo contenido en la clase About AMS
+contenidoWebNoticia <- html_nodes(webpage,'.post-2')
+
+# Pasando la info a texto
+textoNoticia <- html_text(contenidoWebNoticia)
+
+# Viendo a priori la info en la variable textoNoticia
+print(textoNoticia)
+
+# Pregunta: ¿Qué representa el \n?
+
+# Eliminando los \n,comillas("),puntos(.) y comas(,) del texto
+textoNoticia <- gsub("\n","",textoNoticia)
+textoNoticia <- gsub("\"","",textoNoticia)
+textoNoticia <- gsub("[.]","",textoNoticia)
+textoNoticia <- gsub(",","",textoNoticia)
+
+# Viendo a priori la info en la variable textoNoticia
+print(textoNoticia)
+
+# Separando las palabras por espacio
+splitEspacioNoticia <- strsplit(textoNoticia," ")[[1]]
+
+# Pasando todas las palabras a minúsculas
+splitEspacioNoticia <- tolower(splitEspacioNoticia)
+
+# Contando palabras
+unlistNoticias<-unlist(splitEspacioNoticia)
+tablaPalabras<-table(unlistNoticias)
+
+# Pasando la información a un data frame
+dfPalabrasNoticia <- as.data.frame(tablaPalabras)
+
+# Almacenando la información en CSV
+write.csv(dfPalabrasNoticia, file="PalabrasNoticia.csv")
+
+# o en un txt
+write.table(dfPalabrasNoticia, file="PalabrasNoticia.txt")
