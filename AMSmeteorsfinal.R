fbbb68852535656da0971b2cee112e8f42613180
Jpvanreckless <juan.lorcag@utem.cl>
2019-01-24 23:56:28 -0300

    Probando extracción de tablas

--- AMSmeteorsfinal.R
+++ AMSmeteorsfinal.R
@@ -1,8 +1,9 @@
 #==================== AMS.METEORS.ORG ====================#
 
 ##########################################################
-######### Iniciando la extracción de información #########
+##### Iniciando la extracción de información general #####
 ##########################################################
+library(rvest)
 
 # Se asigna la página de ams.meteors.org/firelogs
 webpage<- 'https://www.amsmeteors.org/about/'
@@ -49,3 +50,38 @@ write.csv(dfPalabrasNoticia, file="PalabrasNoticia.csv")
 
 # o en un txt
 write.table(dfPalabrasNoticia, file="PalabrasNoticia.txt")
+
+##########################################################
+######## Iniciando la extracción de avistamientos ########
+##########################################################
+
+# Se asigna la página de ams.meteors.org/firelogs
+Fireball<- 'https://www.amsmeteors.org/members/imo_view/browse_events?country=-1&year=2018&num_report=-99'
+
+Fireball <- read_html(Fireball)
+
+# Extrayendo contenido en la clase Registro general
+tabla <- html_nodes(Fireball,".table-responsive")
+
+# Extrayendo contenido en la clase registro de avistamientos
+tablainfo <- html_nodes(tabla,"table")
+
+# Extraccion informacion tabla 
+tabla1 <- html_table(tablainfo)
+
+# Viendo a priori la info en la variable textoNoticia
+print(tabla1)
+
+# Eliminando los \n,comillas("),puntos(.) y comas(,) del texto
+registro <- gsub("\n","",registro2)
+registro <- gsub("\"","",registro2)
+registro <- gsub("[.]","",registro2)
+registro <- gsub(",","",registro2)
+registro <- gsub("-","",registro2)
+registro <- gsub("'","",registro2)
+registro <- gsub(":","",registro2)
+registro <- gsub(";","",registro2)
+
+# Viendo a priori la info en la variable textoNoticia
+print(registro)
+
