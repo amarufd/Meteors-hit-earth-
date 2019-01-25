7ef3f3975ad6c9c2c693631afb9be129fb1896bd
AMARU FERNANDEZ <afernandez@altiuz.cl>
2019-01-25 01:40:30 -0300

    Extraccion de datos y combinacion con otras tablas de la misma página

--- AMSmeteorsfinal.R
+++ AMSmeteorsfinal.R
@@ -1,11 +1,11 @@
 #==================== AMS.METEORS.ORG ====================#
 
 ##########################################################
-##### Iniciando la extracción de información general #####
+##### Iniciando la extracci?n de informaci?n general #####
 ##########################################################
 library(rvest)
 
-# Se asigna la página de ams.meteors.org/firelogs
+# Se asigna la p?gina de ams.meteors.org/firelogs
 webpage<- 'https://www.amsmeteors.org/about/'
 
 webpage <- read_html(webpage)
@@ -35,53 +35,62 @@ print(textoNoticia)
 # Separando las palabras por espacio
 splitEspacioNoticia <- strsplit(textoNoticia," ")[[1]]
 
-# Pasando todas las palabras a minúsculas
+# Pasando todas las palabras a min?sculas
 splitEspacioNoticia <- tolower(splitEspacioNoticia)
 
 # Contando palabras
 unlistNoticias<-unlist(splitEspacioNoticia)
 tablaPalabras<-table(unlistNoticias)
 
-# Pasando la información a un data frame
+# Pasando la informaci?n a un data frame
 dfPalabrasNoticia <- as.data.frame(tablaPalabras)
 
-# Almacenando la información en CSV
+# Almacenando la informaci?n en CSV
 write.csv(dfPalabrasNoticia, file="PalabrasNoticia.csv")
 
 # o en un txt
 write.table(dfPalabrasNoticia, file="PalabrasNoticia.txt")
 
 ##########################################################
-######## Iniciando la extracción de avistamientos ########
+######## Iniciando la extracci?n de avistamientos ########
 ##########################################################
+fullTable <- data.frame()
 
-# Se asigna la página de ams.meteors.org/firelogs
-Fireball<- 'https://www.amsmeteors.org/members/imo_view/browse_events?country=-1&year=2018&num_report=-99'
+for (i in 1:3) {
+  # Se asigna la p?gina de ams.meteors.org/firelogs
+  Fireball <- paste("https://www.amsmeteors.org/members/imo_view/browse_events?country=-1&year=2018&num_report=-99&page=",i,sep = "")
+  
+  print(Fireball)
+  
+  Fireball <- read_html(Fireball)
+  
+  # Extrayendo contenido en la clase registro de avistamientos
+  tablainfo <- html_nodes(Fireball,"table")
+  
+  # Extraccion informacion tabla 
+  tabla1 <- html_table(tablainfo,fill = TRUE)
+  
+  # Convierte a data.frame
+  dfTabla <- data.frame(tabla1)
+  
+  # Elimina datos malos
+  dfTabla <- dfTabla[-c(1), ]
+  dfTabla$NA.<- NULL
+  dfTabla$NA..1 <- NULL
+  dfTabla$NA..2 <- NULL
+  
+  # Viendo a priori la info en la variable textoNoticia
+  print(dfTabla)
+  
+  fullTable <- rbind(fullTable,dfTabla)
+  
+  # Eliminando elementos repetidos
+  fullTable <- unique(fullTable)
+  
+}
 
-Fireball <- read_html(Fireball)
 
-# Extrayendo contenido en la clase Registro general
-tabla <- html_nodes(Fireball,".table-responsive")
 
-# Extrayendo contenido en la clase registro de avistamientos
-tablainfo <- html_nodes(tabla,"table")
 
-# Extraccion informacion tabla 
-tabla1 <- html_table(tablainfo)
 
-# Viendo a priori la info en la variable textoNoticia
-print(tabla1)
-
-# Eliminando los \n,comillas("),puntos(.) y comas(,) del texto
-registro <- gsub("\n","",registro2)
-registro <- gsub("\"","",registro2)
-registro <- gsub("[.]","",registro2)
-registro <- gsub(",","",registro2)
-registro <- gsub("-","",registro2)
-registro <- gsub("'","",registro2)
-registro <- gsub(":","",registro2)
-registro <- gsub(";","",registro2)
-
-# Viendo a priori la info en la variable textoNoticia
-print(registro)
 
